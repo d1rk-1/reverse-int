f1710dde7f186ae19bc77c514a9fcbba882fc88f
Kostiantyn <70088981+d1rk-1@users.noreply.github.com>
2021-03-15 11:53:34 +0200

    finish reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let j = n.toString().split('').reverse().join('');
+        let w = Number(j);
+        return w;
+    } else if (Math.sign(n) == -1) {
+        let z = -(n);
+        let c = z.toString();
+        let m = c.split('');
+        let k = m.reverse().join('');
+        return Number(k);
+
+    }
+};
